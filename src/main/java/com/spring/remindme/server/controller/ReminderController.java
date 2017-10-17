5b0c62555a6de8bffb03252cb04fe7550bd5acfb
Nikolay Morozov <nmorozov@outlook.com>
2017-10-17 01:30:28 -0700

    added first working application

--- src/main/java/com/spring/remindme/server/controller/ReminderController.java
+++ src/main/java/com/spring/remindme/server/controller/ReminderController.java
@@ -1,18 +1,30 @@
 package com.spring.remindme.server.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import com.spring.remindme.server.entity.Remind;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Date;
+
+@RestController
 @RequestMapping("/remind")
 public class ReminderController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
-    public String getReminder(Model model){
-        return "My reminder";
+    public Remind getReminder(){
+
+        return createMockRemind();
+    }
+
+    private Remind createMockRemind() {
+
+        Remind remind = new Remind();
+        remind.setId(1);
+        remind.setRemindDate(new Date());
+        remind.setTitle("First remind");
+        return remind;
     }
 }
